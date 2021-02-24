99872d4455abd40e9a5d7246ed28d3baced6c611
Anirudh Bhat <56544816+anirudhjl@users.noreply.github.com>
2021-02-24 15:43:44 +0530

    Updated form

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -8,6 +8,11 @@ class Contact extends React.Component {
   }
 
   handleSubmit(event) {
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: encode({ "form-name": "contact", ...this.state }),
+    });
     event.preventDefault();
     console.log(this.state);
     this.props.history.push("/");
@@ -18,12 +23,15 @@ class Contact extends React.Component {
       <div className="container" style={{ padding: "6%" }}>
         <p style={{ paddingBottom: "1%" }}>Have a question/message?</p>
         <form
-          onSubmit={this.handleSubmit}
           className="form-group"
           data-netlify="true"
           name="contact"
           method="POST"
+          onSubmit={this.handleSubmit}
+          data-netlify-honeypot="bot-field"
         >
+          {" "}
+          <input type="hidden" name="form-name" value="contact" />
           <input
             name="email"
             type="email"
