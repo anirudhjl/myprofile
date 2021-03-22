08a4b8d5f3ec8f9c90580c3b81cd5159552d11c2
Anirudh Bhat <56544816+anirudhjl@users.noreply.github.com>
2021-03-22 12:35:55 +0530

    Modified page

--- src/components/skills.jsx
+++ src/components/skills.jsx
@@ -1,14 +1,17 @@
 import React from "react";
 import "./style.css";
-import Resume from "../File/AnirudhJLResume.pdf";
+import Resume from "../File/AnirudhJLresume.pdf";
 
 const Skills = () => {
   return (
     <div className="container" style={{ padding: "10%" }}>
       <p>
-        Languages: HTML, CSS, Sass, JavaScript, C, C#, Python
+        Front end: HTML5, CSS3, Sass, Bootstrap, Tailwind CSS, JavaScript,
+        jQuery, React
         <br />
-        Libraries: Bootstrap, Tailwind CSS, jQuery, React
+        Back end: MySQL, MongoDB, C, C#, Python
+        <br />
+        Dev Tools: Eclipse IDE, VS code, Git, Netlify, Node.js, npm, Jira
         <br />
         <br />
         <a href={Resume} download>
