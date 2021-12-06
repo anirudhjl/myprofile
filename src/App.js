f6775d5dcc723f67c79c6ba29dc305294a50e6ce
Anirudh Bhat <56544816+anirudhjl@users.noreply.github.com>
2021-12-06 22:34:23 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import * as React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Header from "./components/header";
 import Footer from "./components/footer";
