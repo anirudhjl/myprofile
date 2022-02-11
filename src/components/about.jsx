b53a24055c0885bba5a5d9122f01565c2283e04c
Anirudh Bhat <56544816+anirudhjl@users.noreply.github.com>
2022-02-11 15:27:20 +0530

    Modified experience using date

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -1,11 +1,14 @@
 import "./style.css";
 
 const About = () => {
+  const joiningDate = new Date("2019-8-27");
+  let diff = new Date(new Date() - joiningDate);
+  let date = diff.toISOString().slice(0, 4) - 1970 + "." + diff.getMonth();
   return (
     <div className="container" style={{ padding: "10%" }}>
       <p>
         I'm a Software Engineer from Bangalore currently working in Mumbai at{" "}
-        <strong style={{ color: "#007bff" }}>LTI</strong> having 2.5 years of
+        <strong style={{ color: "#007bff" }}>LTI</strong> having {date} years of
         experience with knowledge in libraries like jQuery, React and Spring
         Boot.
       </p>
